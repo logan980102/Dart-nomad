cc33cd8b69c74af8cc6a769a97e3a809b9279323
logan <logan980102@gmail.com>
2025-07-04 23:52:39 +0900

    first dart git

---
+++ main.dart
@@ -0,0 +1,8 @@
+String sayHello(String name) => "Hello $name nice to meet you!";
+num plus(num a, num b) => a + b;
+void main() {
+  print(sayHello('nico'));
+  var first = 10;
+  var sec = 26.6;
+  print(plus(first, sec));
+}
